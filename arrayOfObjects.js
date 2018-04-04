e8b764f6d84f9d94696879ce789b10b0c1adccd9
Dameon1 <dameonmedoza@gmail.com>
2018-04-04 11:53:01 -0700

    updated arrayOf

--- arrayOfObjects.js
+++ arrayOfObjects.js
@@ -3,17 +3,25 @@
 let person1 = {
   name: 'Jeffrey',
   jobTitle: 'Boss',
+  
 };
 
 let person2 = {
   name: 'Dameon',
   jobTitle: 'Dad',
+  boss: 'Jeffrey',
 };
 
 let person3 = {
   name: 'Rich',
   jobTitle: 'Programmer',
+  boss: 'Dameon',
 };
 
 let persons = [person1, person2, person3];
-persons.forEach(person => console.log(`${person.name} is a ${person.jobTitle}`));
+persons.forEach(person => console.log(`${person.jobTitle} ${person.name} ${'boss' in person ? 'reports to ' + person.boss : 'reports to no one'}`));
+
+
+
+
+
