39762035185c00edc3ace4a83b89d7b149d914e2
Dameon1 <dameonmedoza@gmail.com>
2018-04-04 11:00:34 -0700

    second

--- initializer.js
+++ initializer.js
@@ -11,7 +11,7 @@ let loaf = {
   flour: 300,
   water : 210,
   hydration : function(){
-    return (loaf.water/loaf.flour) * 100;
+    return (this.water/this.flour) * 100;
   }
 };
 console.log(loaf.flour);
