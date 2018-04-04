94af5e39d9ae079a1bb8d803b04348d026d857e4
Dameon1 <dameonmedoza@gmail.com>
2018-04-04 13:01:42 -0700

    added find function

--- factoryFunction.js
+++ factoryFunction.js
@@ -19,21 +19,25 @@ function createCharacter(name,nickname,race,origin,attack,defense){
   };
 }
 
-let bilbo =  createCharacter('bilbo','baggins','hobbit','the shire',2,1);
-let gandalf = createCharacter('Gandalf the White','gandalf','Wizard','Middle Earth',10,6);
-//console.log(bilbo.describe());
-console.log(bilbo.evaluateFight(gandalf));
-console.log(gandalf.evaluateFight(bilbo));
+
+
+
+let characters = [
+  createCharacter('bilbo','baggins','hobbit','the shire',2,1),
+  createCharacter('Gandalf the White','gandalf','Wizard','Middle Earth',10,6),
+  createCharacter('Frodo Baggins','frodo','Hobbit','The Shire',3,2),
+  createCharacter('Aragonrn son of Arathorn','aragorn','Man','Dunnedain',6,8),
+  createCharacter('Legolas','legolas','Elf','Woodland Realm',8,5),];
 
 
 
 
+characters.push(createCharacter('dameon','mendoza','human','earth',100,100));
+let foundChar = characters.find(function(character){
+  return  character.nickname === 'aragorn';
+});
 
-
-
-
-
-
+console.log(foundChar.describe());
 
 
 
@@ -59,12 +63,15 @@ console.log(gandalf.evaluateFight(bilbo));
 // that takes in a character object and returns the following string: 
 //"Your opponent takes {x} damage and you receive {y} damage" where x and y are the differences between each characters attack and defense values. If defense exceeds attack, then take zero damage.
 
-// Using array literal syntax, create an array characters that calls your factory function for each character in the table above with the relevant parameters. Your characters array should now have 5 objects in it.
+// Using array literal syntax, 
+//create an array characters that calls your factory function for each character in the
+// table above with the relevant parameters. Your characters array should now have 5 objects in it.
 
 // Add a new character to characters (make up any attributes not provided):
 
 // Arwen Undomiel is a Half-Elf of Rivendell
-// Using the .find() function, retrieve your character nicknamed aragorn from characters and then call his describe method.
+// Using the .find() function, retrieve your character
+// nicknamed aragorn from characters and then call his describe method.
 
 // Using the .filter() function, create a new array from characters that ONLY contains characters of the race Hobbit.
 
